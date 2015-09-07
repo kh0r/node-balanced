2ace8dc6ab8e242c7a2c64f22cbe97cab112d103
mklepp <me@mklepp.com>
2015-09-07 12:13:34 +0200

    Add redis and hit counter

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var http = require('http');
-var PORT = process.env.PORT || 3000;
 var os = require("os");
+var redis = require("redis");
+
+var PORT = process.env.PORT || 3000;
+var KEY_HIT_COUNTER = "KEY_HIT_COUNTER";
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world!\n'+'from: '+os.hostname());
+  client = redis.createClient();
+
+  client.incr(KEY_HIT_COUNTER);
+
+  client.get(KEY_HIT_COUNTER, function (err, value){
+    if (err) throw(err);
+    var response = 'Hello world!\n';
+    response += 'from: ' + os.hostname() + '\n';
+    response += 'hit counter: ' + value;
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(response);
+
+    client.quit();
+  });
+
+
 }).listen(PORT);
 
 console.log('%d listening on %d', process.pid, PORT);
